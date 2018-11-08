b05db897e60c59191fcd0737c7a9e4d78427a6ec
Taras Vitovshchyk <vitovstreet@gmail.com>
2018-11-08 17:20:02 +0200

    error option and transition effect for list elements

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-		<h1>Task List</h1>
-		<form @submit.prevent="addtask" class="add_task">
+		<h1>ToDo List</h1>
+		<form @submit.prevent="addTask" class="add_task">
 			<div class="input_box">
 				<input type="text" v-model="task.name" placeholder="Name task"/>
 			</div>
@@ -9,13 +9,19 @@
 				<input type="text" v-model="task.time" placeholder="Time task"/>
 			</div>
 			<button type="submit" class="t-up btn_add" @click="addTask()">Add task</button>
+			<p class="alert" v-if="showAlert">{{ alertMessage }}</p>
 		</form>
-		<div>
-			<p class="no_task" v-if="!tasks.length">No tasks!</p>
-			<ul v-if="tasks.length" class="todo_list">
+		<div v-if="tasks.length">
+			<transition-group 
+				name="slide"
+				mode="out-in"
+				class="todo_list"
+				tag="ul"
+				key="list"
+			>
 				<li
 					v-for="(task, index) in tasks"
-					:key="task.id"
+					:key="task.name"
 				>
 					<div class="task_cell"><span>Name task:</span> {{ task.name }}</div>
 					<div class="task_cell"><span>Time:</span> {{ task.time }}</div>
@@ -23,8 +29,9 @@
 			        	<i class="fas fa-trash-alt"></i>
 			        </button>
 				</li>
-			</ul>
+			</transition-group>
 		</div>
+		<p class="no_task" v-else>No tasks!</p>
 	</div>	
 </template>
 
@@ -53,22 +60,27 @@ export default {
 			task: {
 				name: '',
 				time: ''
-			}
+			},
+			showAlert: false,
+			allertMessage: '',
 		}
 	},
 	methods: {
 		addTask() { 
-	      if (this.task.name) {
-	        this.tasks.push(this.task);
+	      if (this.task.name && this.task.time) {
+	        this.tasks.push(this.task)
 	        this.task = {
 	          name: '',
 	          time: ''
 	        }
-	      }
+	      } else if(this.task.name =='' && this.task.time ==''){
+	            this.alertMessage = "Please fill the inputs."
+	            this.showAlert = true;
+          }
 		},
 	    removeTask(task, index) {
 	    	this.tasks.splice(index, 1);
-	    },
+	    }
 	}
 }
 
