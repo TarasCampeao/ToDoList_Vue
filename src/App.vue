dbdc50cf70916290caa453c5725b6b6d652e679b
Taras Vitovshchyk <vitovstreet@gmail.com>
2018-11-08 12:54:54 +0200

    start files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<style lang="sass">
+    @import "./assets/style/main.sass"
+    @import "./assets/style/media.sass"
+</style>
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  },
+};
+</script>
+
+
